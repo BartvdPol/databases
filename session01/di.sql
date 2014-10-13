7917cb9dea0ab1def753a0f4d61134aae2a9d9f8
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-13 15:27:01 +0200

    practicum 1

--- session01/di.sql
+++ session01/di.sql
@@ -1,2 +1,2 @@
-/*  4*6=24 regels*/
-SELECT * FROM Docent, Thema WHERE Docent.docent_id = Thema.docent_id;
+/* 1g*/
+SELECT Docent.naam, Thema.naam FROM Docent, Thema WHERE Docent.naam="Dr. Luis" and Docent.docent_id=Thema.docent_id;
