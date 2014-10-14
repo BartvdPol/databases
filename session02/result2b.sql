24dab75db226e81aa5922acc9570adbfec0fa262
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-14 13:09:38 +0200

    stap 1, 2b

--- session02/result2b.sql
+++ session02/result2b.sql
@@ -1,13 +1,12 @@
-/* These first two lines format the SELECT statement output to be more readable. */
-
-/* To see the full list of these SQLite-specific commands, type '.help'.         */
+/*    shunting rule    */
 
 .mode columns
 
 .headers on
 
-SELECT * FROM Person p limit 5
-WHERE EXISTS ( 
-SELECT * FROM Acts a 
-WHERE a.pid = p.pid
+SELECT p.pid FROM Person p
+WHERE EXISTS(
+	SELECT * FROM Acts a, Movie m
+	WHERE a.pid = p.pid AND m.name = 'Back to the Future' AND a.mid=m.mid
+	
 );
