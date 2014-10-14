92b2f49c9e54f3a7a3d372a9ad7a6b8926b27257
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-14 12:23:09 +0200

    stap 2, hiervoor is 2c gebruikt 2d

--- session02/result2d.sql
+++ session02/result2d.sql
@@ -4,4 +4,16 @@
 
 .headers on
 
-Schrijvers die regisseurloze films hebben geschreven except schrijvers die films met regisseur hebben geschreven
+SELECT p.name From Person p
+WHERE EXISTS (
+	SELECT * FROM Writes w /* er bestaat een schrijver p */
+	WHERE  p.pid = w.pid 
+	AND EXISTS (
+		SELECT * FROM Movie m /* er bestaat een film die geschreven is door schrijver p */
+		WHERE m.mid = w.mid
+		AND NOT EXISTS (
+			SELECT * FROM Directs d
+			WHERE d.mid = m.mid
+		)
+	)
+) AND NOT IN schrijvers die films met regisseur hebben geschreven
