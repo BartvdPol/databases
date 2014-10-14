e429b277908f172ad68e6d70f85923134097f75c
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-14 12:44:54 +0200

    stap 3, hiervoor weer 2c gebruikt, maar zonder NOT. 2d

--- session02/result2d.sql
+++ session02/result2d.sql
@@ -16,16 +16,19 @@ WHERE EXISTS (
 			WHERE d.mid = m.mid
 		)
 	)
-) AND NOT EXISTS (SELECT p.name From Person p
+) 
+AND p.name NOT IN(
+SELECT p.name From Person p
 WHERE EXISTS (
 	SELECT * FROM Writes w /* er bestaat een schrijver p */
 	WHERE  p.pid = w.pid 
 	AND EXISTS (
 		SELECT * FROM Movie m /* er bestaat een film die geschreven is door schrijver p */
 		WHERE m.mid = w.mid
-		AND EXISTS ( /* hier is het NOT statement weggehaald om alle schrijvers van films te krijgen met een regisseur */
+		AND EXISTS (
 			SELECT * FROM Directs d
 			WHERE d.mid = m.mid
 		)
 	)
+)
 );
