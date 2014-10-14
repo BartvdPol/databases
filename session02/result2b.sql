120aba93b3a9d1fbae854e40e1cc9ad68fbc15ff
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-14 13:10:40 +0200

    stap 2, 2b

--- session02/result2b.sql
+++ session02/result2b.sql
@@ -4,9 +4,6 @@
 
 .headers on
 
-SELECT p.pid FROM Person p
-WHERE EXISTS(
-	SELECT * FROM Acts a, Movie m
-	WHERE a.pid = p.pid AND m.name = 'Back to the Future' AND a.mid=m.mid
-	
-);
+SELECT p.pid FROM Person p, Acts a, Movie m
+WHERE a.pid = p.pid AND m.name = 'Back to the Future' AND a.mid=m.mid
+;
