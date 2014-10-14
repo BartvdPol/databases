16ca9e011505b2db41a503dad92823bc0c6d444a
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-14 11:55:49 +0200

    ga verder met 2c

--- session02/result2b.sql
+++ session02/result2b.sql
@@ -1,10 +1,13 @@
-/* One point rule */
-SELECT p.pid FROM Person p 
-WHERE EXISTS( 
-	SELECT * FROM Acts a 
-	WHERE a.pid = p.pid 
-	AND 'Back to the Future' IN ( 
-		SELECT * FROM Movie m  –­ the movies exists and a plays in it
-		WHERE m.mid = a.mid 
-	) 
+/* These first two lines format the SELECT statement output to be more readable. */
+
+/* To see the full list of these SQLite-specific commands, type '.help'.         */
+
+.mode columns
+
+.headers on
+
+SELECT * FROM Person p limit 5
+WHERE EXISTS ( 
+SELECT * FROM Acts a 
+WHERE a.pid = p.pid
 );
