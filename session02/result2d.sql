c86d23ba65fa6ea62c17430e66f11476ba3f723a
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-14 12:21:02 +0200

    stap 1, 2d

---
+++ session02/result2d.sql
@@ -0,0 +1,7 @@
+/*    Van welk schrijvers zijn alle films regisseurloos    */
+
+.mode columns
+
+.headers on
+
+Schrijvers die regisseurloze films hebben geschreven except schrijvers die films met regisseur hebben geschreven
