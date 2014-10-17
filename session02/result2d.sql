f38b56a44c3541aeda563c90026fd01606ecff5d
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-17 09:41:15 +0200

    shunting rule 2d

--- session02/result2d.sql
+++ session02/result2d.sql
@@ -11,11 +11,6 @@ SELECT p.name From Person p, Writes w, Movie m
 			WHERE d.mid = m.mid
 		)
 AND p.name NOT IN(
-SELECT p.name From Person p, Writes w, Movie m
-	WHERE  p.pid = w.pid AND m.mid = w.mid
-		AND EXISTS (
-			SELECT * FROM Directs d
-			WHERE d.mid = m.mid
-		)
-;
+SELECT p.name From Person p, Writes w, Movie m, Directs d
+	WHERE  p.pid = w.pid AND m.mid = w.mid AND d.mid = m.mid
 );
