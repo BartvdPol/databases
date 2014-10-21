0d4eae6d131af7b3e2473c8a293266cbbe42a093
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-21 17:03:41 +0200

    3l 2e keer

--- session03/result3k-m.sql
+++ session03/result3k-m.sql
@@ -1,7 +1,11 @@
 INSERT INTO F VALUES
 ('ABC','EF',0,'geen violation, want ABC is de key'),
 ('E','D',0,'geen violation, want E is een key'),
-('D','A',1,'want D is geen key');
+('D','A',0,'geen violation, want D is een key');
 /* pak een van de relaties die in overtreding is, zet al die attributen in een tabel en in de andere tabel de attributen + de linkerkant*/
-CREATE TABLE R1 (E INT, D INT);
-CREATE TABLE R2 (A INT, B INT, C INT, E INT, F INT);
+CREATE TABLE R1 (D INT, A INT);
+/*CREATE TABLE R2 (B INT, C INT, D INT, E INT, F INT);*/
+
+/*dan pak je table R2 en die werk je weer verder uit tot deze drie tabellen. */
+CREATE TABLE R3 (D INT, E INT);
+CREATE TABLE R4 (A INT, B INT, C INT, E INT, F INT);
