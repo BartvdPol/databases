243ab58ebf02e5e37110c6613c87f7bc10c61304
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-21 16:48:19 +0200

    3k

--- session03/result3k-m.sql
+++ session03/result3k-m.sql
@@ -1,4 +1,4 @@
 INSERT INTO F VALUES
-('ABC','EF',0,'geen violation'),
+('ABC','EF',0,'geen violation, want ABC is de key'),
 ('E','D',1,'want E is geen key'),
-('D','A',0,'geen violation');
+('D','A',1,'want D is geen key');
