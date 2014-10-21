5400e0065094d9a3009728f0443f08e4c0ae2fe6
wasikhetmaar <wasikhetmaar@gmail.com>
2014-10-21 16:59:30 +0200

    3l

--- session03/result3k-m.sql
+++ session03/result3k-m.sql
@@ -2,3 +2,6 @@ INSERT INTO F VALUES
 ('ABC','EF',0,'geen violation, want ABC is de key'),
 ('E','D',1,'want E is geen key'),
 ('D','A',1,'want D is geen key');
+
+CREATE TABLE R1 (E INT, D INT);
+CREATE TABLE R2 (A INT, B INT, C INT, E INT, F INT);
